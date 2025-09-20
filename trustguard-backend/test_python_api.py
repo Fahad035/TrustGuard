65f255b73a649b14d608c652fea83b4df02d6175
Md Fahad <fahadjdk345671@gmail.com>
2025-09-20 18:45:11 +0530

    initial commit

--- trustguard-backend/test_python_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:5001/analyze'
-data = {"text": "This is a test sentence about fake news."}
-
-response = requests.post(url, json=data)
-print("Status code:", response.status_code)
-print("Response:", response.json())
